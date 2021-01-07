598b6029aab3775c2e8140f63b90a9f03043315e
Rafael Dalzotto <rafaeldalzotto2016@gmail.com>
2021-01-07 09:31:36 -0300

    Modified

--- eeprom_cleaning.h
+++ eeprom_cleaning.h
@@ -11,7 +11,7 @@ void Make_Sure_EEPROM_is_Clear(uint8_t BYTE0, uint8_t BYTE1)
 	int length = EESIZE;
 	if (EEPROM.read(length-2) != BYTE0 || EEPROM.read(length-1) != BYTE1)
 	{
-		DEBUG("EEPROM Error")
+		DEBUG("EEPROM Error, cleaning...")
 		for (int i = 0 ; i < length; i++) {
 			#if defined( ESP_H ) || defined( _HELTEC_H_ )
 			if (EEPROM.read(i) != 0)
@@ -23,16 +23,22 @@ void Make_Sure_EEPROM_is_Clear(uint8_t BYTE0, uint8_t BYTE1)
     		EEPROM.update(i, 0);
 			#endif
   		}
+		DEBUG("EEPROM cleared")
 		#if defined( ESP_H ) || defined( _HELTEC_H_ )
 		EEPROM.write(length-2, BYTE0);
 		EEPROM.commit();
 		EEPROM.write(length-1, BYTE1);
 		EEPROM.commit();
+
+		delay(100);
+		ESP.restart();
 		#else
 		EEPROM.update(length-2,BYTE0);
 		EEPROM.update(length-1,BYTE1);
+
+ 		delay(100);
+		asm("jmp 0x0000");
 		#endif
-		DEBUG("EEPROM cleared")
 	}
 	else
 	{
